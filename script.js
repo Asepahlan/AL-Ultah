e8f9a5d54af5b31eacdd7655b29ef7e8fa3578f2
Asepahlan <109494764+Asepahlan@users.noreply.github.com>
2024-11-02 22:58:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function revealAnswer() {
+    const riddleSection = document.getElementById("riddle-section");
+    const birthdaySection = document.getElementById("birthday-section");
+    
+    riddleSection.style.display = "none";
+    birthdaySection.style.display = "block";
+  }
+  
